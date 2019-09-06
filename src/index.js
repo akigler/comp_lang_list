ecb6a75ff10ef955fa2cfa916f7762c3065f302e
root <akigler1@binghamton.edu>
2019-09-06 12:19:21 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const langPercent = [
+  ['javascript', 67.8],
+  ['html/css', 63.5],
+  ['sql', 54.4],
+  ['python', 41.7],
+  ['java', 41.1],
+  ['bash/shell/powershell', 36.6],
+  ['c#', 31],
+  ['php', 26.4],
+  ['c++', 23.5],
+  ['typescript', 21.2]
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function LangList(props) {
+  const langs = props.langs;
+  const listItems = langs.map((lang, index) =>
+    <li>{`${langs[index][0]} - %${langs[index][1]}`}</li>
+  );
+  return (
+    <ol>{listItems}</ol>
+  );
+}
+ReactDOM.render(<LangList langs={langPercent} />, document.getElementById('root'));
